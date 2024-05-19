688303f5f68a751783af455fb1fcf6840d7c8c1f
Alexander Wan <alexwan@berkeley.edu>
2024-05-19 19:18:36 -0400

    fix answer parsing bug

--- mwp/model/prompts.py
+++ mwp/model/prompts.py
@@ -9,23 +9,20 @@ Explain your reasoning step by step. Write your final answer at the end. Prefix 
 
 basic_cot_prompt_fs = ("There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?", "There are 15 trees originally. Then there were 21 trees after some more were planted. So there must have been 21 - 15 = 6. The answer is 6")
 
-def basic_cot_prompt_parse(raw_output: str) -> int:
-    # get everything after prefix
-    match = re.search(r'answer is(.*)', raw_output)
+def basic_cot_prompt_parse(raw_output):
+    match = re.search(r'answer is(.*)', raw_output, re.S)
     if not match:
         return None
     
     match_str = match.group(1)
-    print('parser str:', match_str)
 
     # find an integer in the string
-    match = re.findall(r'\d+', match_str)
+    match = re.findall(r'\d+', match_str, re.S)
     if not match:
         return None
-    
+
     val = int(match[-1])
-    print('parser int:', val)
-    
+
     return val
 
 
